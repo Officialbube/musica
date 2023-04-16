07e5d049cc9cc4c9dde6d70bc0e3f078a1c5e1c6
officialbube <ebubenkereebube@gmail.com>
2023-04-16 11:45:28 +0100

    updated

--- src/components/header.js
+++ src/components/header.js
@@ -4,8 +4,16 @@ import './header.css';
 const Header = ()=>{
     return(
         <div className="header">
-            <div className="header-container">                
-                <img src={"./assets/icons/logo.png"} alt="logo" />
+            <div className="header-container"> 
+                <div className="header-left">
+                    <div className="burger">
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <img src={"./assets/icons/logo.png"} alt="logo" />
+                </div>              
+                
+                <i class="fa fa-magnifying-glass glass-mobile"></i>
                 <div className="search-input">
                     <i class="fa fa-magnifying-glass"></i>
                     <input type="search" placeholder="Search Artist"/>
